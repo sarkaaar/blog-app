a2226a3bfc50d8449d7f0ef2bf5e3099073ebc86
Abdul Wali <waliabdul144@gmail.com>
2025-01-27 23:37:50 +0500

    chat front end

--- UI/src/pages/Homepage.tsx
+++ UI/src/pages/Homepage.tsx
@@ -1,10 +1,25 @@
 import Navbar from "./components/Navbar";
+import Chat_component from "./components/Chat_component";
+
 
 export default function Homepage() {
   return (
     <div>
-        <Navbar />
+      <Navbar />
       <h1>This is the homepage</h1>
+
+      <div className="flex justify-center">
+        <textarea className="border-2 border-emerald-800 w-96" />
+        <button className="border-2 border-emerald-800">Send</button>
+      </div>
+
+
+      <div>
+        <h1>Thread</h1>
+      </div>
+
+      <Chat_component />
+
     </div>
   );
 }
